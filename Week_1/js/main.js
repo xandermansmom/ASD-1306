9f9d4aa4c775566275552138d0d15501322a3e1e
Michele Laramore <xandermansmom@fullsail.edu>
2013-06-05 19:14:05 -0500

    First commit

---
+++ Week_1/js/main.js
@@ -0,0 +1,11 @@
+//Michele Laramore
+//ASD 1306
+
+$('#home').on('pageinit', function(){
+    //code needed for home page goes here
+});
+    
+ 
+  $('#add').on('pageinit', function(){
+    //when add button is pushed, code is activated to post form data to view page
+  });
